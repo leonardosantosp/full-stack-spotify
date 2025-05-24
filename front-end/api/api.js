bf46c71f2576b638aa780e1e8d6a8977e8e331da
leonardosantosp <leo040604@gmail.com>
2025-05-24 10:44:38 -0300

    fix: remove / from url connection API

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 // Fetch ou Axios
 import axios from 'axios'
 
-const URL = 'https://full-stack-spotify-api-production.up.railway.app/'
+const URL = 'https://full-stack-spotify-api-production.up.railway.app'
 
 export const getSongs = async () => {
   const responseSongs = await axios.get(`${URL}/songs`)
